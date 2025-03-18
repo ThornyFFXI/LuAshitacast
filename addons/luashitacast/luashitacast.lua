f6ea357b033812b8a2498b1c6c45a23be7f0010c
ThornyFFXI <10786796+ThornyFFXI@users.noreply.github.com>
2025-03-18 11:21:38 -0400

    2.05 -change callback aliases so that they aren't as easily overwritten by copy/paste code

--- addons/luashitacast/luashitacast.lua
+++ addons/luashitacast/luashitacast.lua
@@ -1,6 +1,6 @@
 addon.name      = 'LuAshitacast';
 addon.author    = 'Thorny';
-addon.version   = '2.04';
+addon.version   = '2.05';
 addon.desc      = 'A lua-based equipment swapping system for Ashita';
 addon.link      = 'https://github.com/ThornyFFXI/LuAshitacast';
 
@@ -21,7 +21,7 @@ gPacketHandlers      = require('packethandlers');
 gSetDisplay          = require('setdisplay');
 gPreservedGlobalKeys = T{};
 
-ashita.events.register('load', 'load_cb', function ()
+ashita.events.register('load', 'luashitacast_load_cb', function ()
     --Create a list of all globals the ashita environment has created.
     --This will be used to clear all leftover globals when loading a new profile.
     for key,_ in pairs(_G) do
@@ -30,19 +30,19 @@ ashita.events.register('load', 'load_cb', function ()
     gState.Init();
 end);
 
-ashita.events.register('d3d_present', 'mobdb_main_render', function()
+ashita.events.register('d3d_present', 'luashitacast_render', function()
     gConfigGUI:Render();
     gSetDisplay:Render();
 end);
 
-ashita.events.register('packet_in', 'packet_in_cb', function (e)
+ashita.events.register('packet_in', 'luashitacast_packet_in_cb', function (e)
     gPacketHandlers.HandleIncomingPacket(e);
 end);
 
-ashita.events.register('packet_out', 'packet_out_cb', function (e)
+ashita.events.register('packet_out', 'luashitacast_packet_out_cb', function (e)
     gPacketHandlers.HandleOutgoingPacket(e);
 end);
 
-ashita.events.register('command', 'command_cb', function (e)
+ashita.events.register('command', 'luashitacast_command_cb', function (e)
     gCommandHandlers.HandleCommand(e);
 end);
